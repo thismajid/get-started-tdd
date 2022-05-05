dd651d42a43ce4f029f02f3a13a6d1ea52b7d8ad
thismajid <mkhorshidian72@gmail.com>
2022-05-05 10:38:39 +0430

    add reverse number function

--- reverseNumber/reverseNumber.js
+++ reverseNumber/reverseNumber.js
@@ -1,3 +1,14 @@
-function reverseNumber(num) {}
+function reverseNumber(num) {
+  let rev = 0;
+  let lastDigit;
+
+  while (num != 0) {
+    lastDigit = num % 10;
+    rev = rev * 10 + lastDigit;
+    num = Math.floor(num / 10);
+  }
+
+  return rev;
+}
 
 module.exports = reverseNumber;
