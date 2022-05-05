b085ea88f9477cf6abf07d9c89a118c1401dce03
thismajid <mkhorshidian72@gmail.com>
2022-05-05 10:31:26 +0430

    add first functionallity test

--- reverseNumber/reverseNumber.test.js
+++ reverseNumber/reverseNumber.test.js
@@ -9,3 +9,9 @@ describe("existance of a function", () => {
     expect(typeof reverseNumber).toEqual("function");
   });
 });
+
+describe("functionallity", () => {
+  it("return 54321 for input of 12345", () => {
+    expect(reverseNumber(12345)).toEqual(54321);
+  });
+});
