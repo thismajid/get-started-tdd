b641650adefaa15425bd23bdc6f7e1e850df2b16
thismajid <mkhorshidian72@gmail.com>
2022-05-05 10:32:43 +0430

    add second functionallity test

--- reverseNumber/reverseNumber.test.js
+++ reverseNumber/reverseNumber.test.js
@@ -14,4 +14,8 @@ describe("functionallity", () => {
   it("return 54321 for input of 12345", () => {
     expect(reverseNumber(12345)).toEqual(54321);
   });
+
+  it("return 87653 for input of 35678", () => {
+    expect(reverseNumber(35678)).toEqual(87653);
+  });
 });
