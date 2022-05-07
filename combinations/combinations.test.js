a1559b0f7ef99f0123636c9b6e2f890b641c81dc
thismajid <mkhorshidian72@gmail.com>
2022-05-07 19:18:10 +0430

    add second functionallity test

--- combinations/combinations.test.js
+++ combinations/combinations.test.js
@@ -14,4 +14,8 @@ describe("functionallity", () => {
   it("return 'd,o,do,g,dg,og,dog' for input of 'dog'", () => {
     expect(combinations("dog")).toEqual("d,o,do,g,dg,og,dog");
   });
+
+  it("return 'c,a,t,ca,ct,at,cat' for input of 'cat'", () => {
+    expect(combinations("cat")).toEqual("d,o,do,g,dg,og,dog");
+  });
 });
